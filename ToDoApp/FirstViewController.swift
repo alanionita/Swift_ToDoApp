91348d52cf308b896e9b76001ab72893a8982dbb
alanionita <a.g.ionita@gmail.com>
2018-01-30 14:20:59 +0000

    adds the completed first pane with a dummy array of tasks

--- ToDoApp/FirstViewController.swift
+++ ToDoApp/FirstViewController.swift
@@ -8,8 +8,32 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UITabBarDelegate, UITableViewDataSource  {
 
+    var list = ["buy milk", "run 5miles", "find Wally", "plant my plants"]
+    
+    @IBOutlet weak var myTableView: UITableView!
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return (list.count)
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "cell")
+            cell.textLabel?.text = list[indexPath.row]
+            return(cell)
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        
+        if editingStyle == UITableViewCellEditingStyle.delete
+        {
+            self.list.remove(at: indexPath.row)
+            myTableView.reloadData()
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +43,5 @@ class FirstViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
