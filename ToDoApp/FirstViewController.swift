b7dc866840cec955b9d7515850e945d245ff1f99
alanionita <a.g.ionita@gmail.com>
2018-01-30 13:16:52 +0000

    Initial Commit

---
+++ ToDoApp/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  ToDoApp
+//
+//  Created by Alan Ionita on 30/01/2018.
+//  Copyright © 2018 Alan Ionita. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
